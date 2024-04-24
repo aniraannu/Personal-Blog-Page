fb7f588d1f5e8f7af8dadbc4617e7383bfd4a754
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-04-23 22:11:39 -0700

    added local storage to store the blog inputs from form

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,3 +3,30 @@ const userNameInput = document.getElementById('name');
 const titleInput = document.getElementById('title');
 const contentInput = document.getElementById('content');
 const submitButton = document.getElementById('submit');
+const msgDiv = document.querySelector('#msg');
+
+//Display Warning Message
+function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute('class', type);
+}
+
+//write function to save data to localStorage while submitting the form
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const blogEntry = {
+        userName: userNameInput.value,
+        title: titleInput.value,
+        content: contentInput.value.trim(),
+    };
+    if (blogEntry.userName === '') {
+        displayMessage('error', 'Username cannot be blank');
+    } else if (blogEntry.title === '') {
+        displayMessage('error', 'Title cannot be blank');
+    } else if (blogEntry.content === '') {
+        displayMessage('error', 'Content cannot be blank');
+    } else {
+        displayMessage('success', 'Blog post added successfully');
+        localStorage.setItem('blogEntry', JSON.stringify(blogEntry));
+    }
+});
