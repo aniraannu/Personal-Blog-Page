5c6c09c5cc68eeb2e095bb93d1a7d26ddbaec3d9
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-04-23 20:23:12 -0700

    added the toggle bar for swithching themes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-//Inputs from the form
-const userNameInput = document.getElementById('name');
-const titleInput = document.getElementById('title');
-const contentInput = document.getElementById('content');
-const submitButton = document.getElementById('submit');
+//Theme Switcher button
+//get the theme switch elemnt using ID
+const themeSwitcher = document.querySelector('#checkbox');
+//define the container
+const container = document.querySelector('.container');
+//let default mode be dark
+let mode = 'dark';
+//function to switch themes
+themeSwitcher.addEventListener('click', function(){
+    if (mode === 'dark'){
+        mode = 'light';
+    container.setAttribute('class', 'light');
+    }
+    else {
+        mode = 'dark';
+        container.setAttribute('class', 'dark');
+      }
+});
